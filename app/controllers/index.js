53dd005e0881b3e7b1393c14ea34a049d952bcb5
曾志豪 <>
2018-04-03 13:46:49 +0800

    fix checkbox has value

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -4,11 +4,16 @@ export default Controller.extend({
   todoInput: '',
   actions: {
     addTodo (text) {
+      const todoInput = this.get('todoInput').trim()
+      if (!todoInput) {
+        return
+      }
       const payload = {
         text
       }
       const todo = this.get('store').createRecord('todo', payload)
       todo.save()
+      this.set('todoInput', '')
     }
   }
 })
