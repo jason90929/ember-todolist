17e63ba5641acac0ddc1b249661aad0185193581
曾志豪 <>
2018-04-03 11:08:00 +0800

    can edit text now

--- app/components/todo-item.js
+++ app/components/todo-item.js
@@ -1,10 +1,38 @@
 import Component from '@ember/component';
 
 export default Component.extend({
+  isEditing: false,
+  editingText: '',
+
   actions: {
     toggleComplete (todo) {
-      console.log('todo.isCompleted', this.get('todo.isCompleted'))
       this.set('todo.isCompleted', !this.get('todo.isCompleted'))
+    },
+
+    showEdit () {
+      this.set('isEditing', true)
+      this.set('editingText', this.get('todo.text'))
+    },
+
+    closeEdit () {
+      this.set('isEditing', false)
+      this.set('editingText', '')
+    },
+
+    onkeydown (e) {
+      if (e.keyCode === 13) {
+        this.send('saveEdit')
+      }
+    },
+
+    saveEdit () {
+      this.set('isEditing', false)
+      this.set('todo.text', this.get('editingText'))
+      this.set('editingText', '')
+    },
+
+    editing (e) {
+      this.set('editingText', e.target.value)
     }
   }
 });
